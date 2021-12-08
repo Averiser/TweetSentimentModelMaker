934e7e3b523fac26ccfa9f668044150e67443d40
Averiser <averiser@gmail.com>
2021-12-09 00:24:37 +0300

    second commit - 353 Using CreateML for Natural Language Processing

--- Contents.swift
+++ Contents.swift
@@ -8,6 +8,15 @@ let(trainingData, testingData) = data.randomSplit(by: 0.8, seed: 5)
 let sentimentClassifier = try MLTextClassifier(trainingData: trainingData, textColumn: "text", labelColumn: "class")
 
 let evaluationMetrics = sentimentClassifier.evaluation(on: testingData, textColumn: "text", labelColumn: "class")
-//
 
 let evaluationAccuracy = (1.0 - evaluationMetrics.classificationError) * 100
+
+let metadata = MLModelMetadata(author: "Alex Averiser", shortDescription: "A model trained to classify sentiment on Tweets", version: "1.0")
+
+try sentimentClassifier.write(to: URL(fileURLWithPath: "/Users/mymacbook/Downloads/TweetSentimentClassifier.mlmodel"))
+
+try sentimentClassifier.prediction(from: "@Apple is a terrible company!")
+
+try sentimentClassifier.prediction(from: "I just found the best restaurant ever, and it's @Duck&Waffle!")
+
+try sentimentClassifier.prediction(from: "I think @CocaCola ads are just OK.") 
